1a6c457b5a040ce0a44ae3a227553d1affe8f67d
Gabriel das Neves <galvesdasneves@gmail.com>
2022-08-12 14:01:32 -0300

    preparing to js implementation

---
+++ calculadora-galaxia/script.js
@@ -0,0 +1,14 @@
+const resultScreen = document.querySelector('#painel');
+let operation;
+
+
+function botao(val){
+    operation = resultScreen.value += val
+}
+
+function clean(){
+    resultScreen.value = ""
+}
+
+
+
